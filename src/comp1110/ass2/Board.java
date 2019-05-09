3bd43b5be49651824ed83330678590cb51cb44b9
Siqi Gu <u6359309@anu.edu.au>
2019-05-09 19:59:27 +1000

    part of game board

--- src/comp1110/ass2/Board.java
+++ src/comp1110/ass2/Board.java
@@ -4,6 +4,18 @@ import comp1110.ass2.Dice;
 import comp1110.ass2.Tile;
 
 public class Board {
+    /* constants describing the shape of the board */
+    public static final int NUM_ROWS = 7;
+    public static final int NUM_COLS = 7;
+    public static final int NUM_SPACES = 26; // number of spaces on board
+    public int col;
+    public int row;
+
+
+    public Board(int col,int row){
+        this.col = col;
+        this.row = row;
+    }
 
     public Tile[][] board = new Tile[7][7];
 
--- src/comp1110/ass2/gui/Game.java
+++ src/comp1110/ass2/gui/Game.java
@@ -1,37 +1,88 @@
 package comp1110.ass2.gui;
 
 
+import comp1110.ass2.Board;
+import comp1110.ass2.Dice;
+import comp1110.ass2.RailroadInk;
 import javafx.application.Application;
 
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.Group;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Priority;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
+import static comp1110.ass2.gui.Viewer.postionImage;
 
 
 public class Game extends Application {
-    //Game board on screen
-    private final int ScreenX = 1024;
-    private final int ScreenY = 768;
-    //node groups
+
+    /* board layout */
+    private static final double DICE_HEIGHT = 65;
+    private static final double DICE_WIDTH = 65;
+
+    private static final int MARGIN_X = (int) DICE_HEIGHT;
+    private static final int BOARD_X = (int) DICE_HEIGHT * 4;
+    private static final int MARGIN_Y = (int) (DICE_HEIGHT * 0.5);
+    private static final int BOARD_Y = MARGIN_Y;
+    private static final int BOARD_WIDTH = Board.NUM_COLS * (int) DICE_HEIGHT;
+    private static final int GAME_WIDTH = 2 * BOARD_X + BOARD_WIDTH;
+    private static final int BOARD_HEIGHT = (int) (Board.NUM_ROWS * DICE_HEIGHT);
+    private static final int PIECE_AREA_HEIGHT = (int) (2 *Dice.MAX_DICE_WIDTH* DICE_HEIGHT);
+    private static final int GAME_HEIGHT = 2 * MARGIN_Y + BOARD_HEIGHT + PIECE_AREA_HEIGHT;
+
+    /* color the underlying board */
+    private static final Paint SUBBOARD_FILL = Color.DARKGREY;
+    private static final Paint SUBBOARD_STROKE = Color.GREY;
+
+    /* where to find media assets */
+    private static final String URI_BASE = "assets/";
+
+    private Pane pane = new Pane();
+    // node groups
     private final Group root = new Group();
     private final Group solution = new Group();
     private final Group board = new Group();
     private final Group controls = new Group();
-    private final Group dices = new Group();
+    private final Group pieces = new Group();
+    // The message on completion
+    private final Text completionText = new Text("Well done!");
+    // The grid for the game
+
+    public static final int rows = 7;
+    public static final int columns = 7;
+    // Import dices
+        public void makeDices(){
+            FlowPane flowPane = new FlowPane();
+
+    }
+
+
+
+
+
+
 
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         primaryStage.setTitle("Game");
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.show();
 
 
 
